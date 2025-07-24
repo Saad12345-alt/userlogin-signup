6f9695b39011ee8a0e5e9da983c1f63da9a0416d
Saad12345-alt <Saadniazi70@gmail.com>
2025-07-24 23:57:54 +0400

    Update server.js
    idk for saftey

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/userlogin", {
+mongoose.connect("mongodb://localhost:SAFTEYYY", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
